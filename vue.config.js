47a114a8b7f998bdc6fbc88b97c9f1402020b16f
ELBS7 <w19187680017@outlook.com>
2026-05-30 13:04:09 +0800

    更改促销活动

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 // vue.config.js
-const { defineConfig } = require('@vue/cli-service')
-
+module.exports={
+  devServer:{
+    port:8081,
+    proxy:{
+      '/api':{
+        target:'http://localhost:8082',
+        changeOrigin:true,
+        pathRewrite:{ '^/api':'' }
+      }
+    }
+  }
+}
