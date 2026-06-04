36055e2359ee9c0868e32e523e3197b6d8b290e3
ELBS7 <w19187680017@outlook.com>
2026-06-04 21:22:54 +0800

    更新促销

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-// vue.config.js
-module.exports={
-  devServer:{
-    port:8081,
-    proxy:{
-      '/api':{
-        target:'http://localhost:8082',
-        changeOrigin:true,
-        pathRewrite:{ '^/api':'' }
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8082/fruit-backend',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
       }
     }
   }
